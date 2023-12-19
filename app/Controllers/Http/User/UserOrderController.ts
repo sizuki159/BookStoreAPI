89752c8d043f8958f3ec7fa64537d9c73de64fa4
sizuki159 <luahantinhthu9@gmail.com>
2023-12-19 15:16:21 +0700

    deploy

--- app/Controllers/Http/User/UserOrderController.ts
+++ app/Controllers/Http/User/UserOrderController.ts
@@ -238,6 +238,9 @@ export default class UserOrderController {
             }
         } catch (e) {
             return response.serviceUnavailable({
+                message: e.message
+            })
+            return response.serviceUnavailable({
                 message: 'Có lỗi hệ thống xảy ra.'
             })
         }
