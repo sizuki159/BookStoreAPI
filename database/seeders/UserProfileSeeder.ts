36940e33fc9de73b73e29c86eefaf0148b1cf793
QuachDieuKhanh <quachdieukhanh@gmail.com>
2023-12-24 11:29:38 +0700

    avatar

--- database/seeders/UserProfileSeeder.ts
+++ database/seeders/UserProfileSeeder.ts
@@ -7,7 +7,7 @@ export default class extends BaseSeeder {
         await UserProfile.createMany([
             {
                 "userId": 1,
-                "avatarSource": "https://sachviet.sgp1.digitaloceanspaces.com/user_avatar/3/3_avatar_img_id_512560",
+                "avatarSource": "https://sachviet.sgp1.digitaloceanspaces.com/user_avatar/1/1_avatar_img_id_519755",
                 "avatarLocation": "user_avatar/3/3_avatar_img_id_512560",
                 "firstname": "Nguyễn",
                 "lastname": "Thành Trung",
