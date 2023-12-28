94f943dcf4fc34a6fccc32dbf968b9b00a98d768
sizuki159 <luahantinhthu9@gmail.com>
2023-12-28 12:00:10 +0700

    edit flash sale seeder

--- database/seeders/FlashSaleSeeder.ts
+++ database/seeders/FlashSaleSeeder.ts
@@ -5,10 +5,21 @@ import { DateTime } from 'luxon'
 export default class extends BaseSeeder {
     public async run() {
         // Write your database queries inside the run method
-        for(let day = 12; day <= 31; day++) {
+
+        // Tháng 12 năm 2023
+        for (let day = 28; day <= 31; day++) {
             await FlashSale.create({
                 eventName: `Flash sale tưng bừng ${day}/12/2023`,
                 eventDate: DateTime.fromFormat(`${day}-12-2023`, 'dd-MM-yyyy'),
+            })
+        }
+
+        // Ngày 1 đến 10 tháng 1 năm 2024
+        for (let day = 1; day <= 10; day++) {
+            const formattedDay = day.toString().padStart(2, '0');
+            await FlashSale.create({
+                eventName: `Flash sale tưng bừng ${formattedDay}/01/2024`,
+                eventDate: DateTime.fromFormat(`${formattedDay}-01-2024`, 'dd-MM-yyyy'),
             })
         }
     }
