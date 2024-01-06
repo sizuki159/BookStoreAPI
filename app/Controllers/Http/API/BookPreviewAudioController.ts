dcd6f82be07b0aeeef1a582a0850d35ff3642991
sizuki159 <luahantinhthu9@gmail.com>
2024-01-06 09:46:49 +0700

    audio trycatch

--- app/Controllers/Http/API/BookPreviewAudioController.ts
+++ app/Controllers/Http/API/BookPreviewAudioController.ts
@@ -4,17 +4,37 @@ import Book from 'App/Models/Book'
 export default class BookPreviewAudioController {
     // Hàm tạo link audio mô tả sách
     public async getAudioDesc({ params, view }: HttpContextContract) {
-        const slug = params.slug
+        try {
+            const slug = params.slug
 
-        const book = await Book.query()
-            .where('slug', slug)
-            .preload('images', (imgQuery) => {
-                imgQuery.groupLimit(1)
+            const book = await Book.query()
+                .where('slug', slug)
+                .preload('images', (imgQuery) => {
+                    imgQuery.groupLimit(1)
+                })
+                .first()
+
+            if (!book) {
+
+                return view.render('book_preview_audio', {
+                    bookName: 'Sách không tồn tại trên hệ thống',
+                    bookImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+                    descArr: ['Hệ thống không tìm thấy sách này theo yêu cầu của bạn'],
+                    bookDesc: 'Hệ thống không tìm thấy sách này theo yêu cầu của bạn'
+                })
+            }
+
+            const desc = book.desc
+            const descArr = this.catChuoi(desc)
+            const bookImg = book.images.length > 0 ? book.images[0].imageSource : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png'
+
+            return view.render('book_preview_audio', {
+                bookName: book.name,
+                bookDesc: book.desc,
+                bookImg,
+                descArr
             })
-            .first()
-
-        if (!book) {
-
+        } catch {
             return view.render('book_preview_audio', {
                 bookName: 'Sách không tồn tại trên hệ thống',
                 bookImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
@@ -22,17 +42,6 @@ export default class BookPreviewAudioController {
                 bookDesc: 'Hệ thống không tìm thấy sách này theo yêu cầu của bạn'
             })
         }
-
-        const desc = book.desc
-        const descArr = this.catChuoi(desc)
-        const bookImg = book.images.length > 0 ? book.images[0].imageSource : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png'
-
-        return view.render('book_preview_audio', {
-            bookName: book.name,
-            bookDesc: book.desc,
-            bookImg,
-            descArr
-        })
     }
 
     private catChuoi(input: string): string[] {
